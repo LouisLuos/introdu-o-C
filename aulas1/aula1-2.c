3e797b9d1309933d3e5552fd4b4171ed3674be33
Luiz Henrique <luiz.contato.1000@gmail.com>
2025-07-27 16:04:04 -0300

    first commit

---
+++ aulas1/aula1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Olá Mundo1\n");
+    printf("Olá Mundo2\n");
+    printf("Olá Mundo3\n");
+    printf("Olá Mundo4\n");
+    printf("Olá Mundo5\n");
+}
---
+++ aulas2/aula2.2/aula1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 0;
+
+    printf("Sua idade inicial é: %d. \n", idade);
+    printf("Digite sua idade atual\n");
+    scanf("%d", &idade);
+
+    printf("Sua idade atual é %d.\n", idade);
+}
